bd2af70ea39bda32777c95e42256c8b8f102c323
github username <github email address>
2019-08-29 13:46:04 -0600

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,39 +1,37 @@
 require "pry"
 
-def all_season
+def all_seasons
   data.values.flatten
 end
 
+
 def get_first_name_of_season_winner(data, season)
-  data[season].find do |s|
-    s["status"] = "Winner"
+  data[season].find do |contestant|
+    contestant["status"] == "Winner"
   end ["name"].split(" ").first
 end
 
-
-def get_contestant_name(data, occupation)
-  all_season.find do |job|
-    job["occupation"] == occupation
+def get_contestant_name (data, occupation)
+  all_seasons.find do |contestant|
+    contestant["occupation"] == occupation
   end ["name"]
 end
 
 def count_contestants_by_hometown(data, hometown)
-  all_season.count do |home|
-    home["hometown"] == hometown
-  end
+  all_seasons.count { |contestant| contestant["hometown"] == hometown}
 end
 
 def get_occupation(data, hometown)
-  all_season.find do |home|
-    home["hometown"] == hometown
+  all_seasons.find do |contestant| 
+    contestant["hometown"] == hometown
   end ["occupation"]
-end
+end 
 
 def get_average_age_for_season(data, season)
-      age = 0
-      data[season].each do |people|
-        age += people["age"].to_i
-      end
-        average = age.to_f/data[season].length
-        average.round
+  age = 0
+  data[season].each do |contestant|
+   age += contestant["age"].to_i
+  end
+  average = age.to_f/data[season].length
+  average.round
 end
